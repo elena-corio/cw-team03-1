bc07c5be70d7f9edaa80c6e621c2828525e7591a
elena-code-star <elena.corio@students.iaac.net>
2026-02-03 00:56:57 +0100

    Replace collection with base - collection not found

--- src/domain/create_data_structure.py
+++ src/domain/create_data_structure.py
@@ -1,11 +1,15 @@
-from specklepy.objects.models.collections import Collection
+#from specklepy.objects.models.collections import Collection
+from specklepy.objects import Base
 
 
 def create_data_structure(old_breps, new_brep):
     # Create new Collections
-    old_modules = Collection(name="Old Modules")
-    new_module = Collection(name="New Module")
-    speckle_model = Collection(name="Speckle Model")
+    old_modules = Base()
+    old_modules.name = "Old Modules"
+    new_module = Base()
+    new_module.name = "New Module"
+    speckle_model = Base()
+    speckle_model.name = "Speckle Model"
     
     #Add Elements to Collections
     old_modules.elements = old_breps
