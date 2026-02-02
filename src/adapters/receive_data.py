ecc1496cca6b5102e1db0c0f9a13c4ee611d440a
elena-code-star <elena.corio@students.iaac.net>
2026-02-02 23:13:59 +0100

    divide get latest version from receive data

--- src/adapters/receive_data.py
+++ src/adapters/receive_data.py
@@ -1,17 +1,7 @@
 from specklepy.api import operations
-from config import PROJECT_ID, SOURCE_MODEL_ID
 
-def receive_data(client, transport):
-    # Get the latest version
-    versions = client.version.get_versions(SOURCE_MODEL_ID, PROJECT_ID, limit=1)
-    if not versions.items:
-        print("No versions found.")
-        return
-    
-    latest_version = versions.items[0]
-    print(f"✓ Fetching version: {latest_version.id}")
-    
+def receive_data(version, transport):
     # Receive the full data tree
-    data = operations.receive(latest_version.referenced_object, transport)
+    data = operations.receive(version.referenced_object, transport)
 
     return data
