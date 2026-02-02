48b188189cc903932e21dd63cde7336eac5958bd
elena-code-star <elena.corio@students.iaac.net>
2026-02-03 00:55:44 +0100

    Get existing model instead of creating new one

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 
 WORKSPACE_ID = os.getenv("WORKSPACE_ID", "a1cd06bae2")
 PROJECT_ID = os.getenv("PROJECT_ID", "128262a20c")
-SOURCE_MODEL = os.getenv("SOURCE_MODEL", "a1014e4b32")
-MODEL_NAME = os.getenv("MODEL_NAME", "homework/session03/team_03.1")
+SOURCE_MODEL_ID = os.getenv("SOURCE_MODEL_ID", "a1014e4b32")
+TARGET_MODEL_ID = os.getenv("TARGET_MODEL_ID", "39d99ae41a")
 MODEL_DESCRIPTION = os.getenv("MODEL_DESCRIPTION", "Model created for CW Team 03.1 Session 03")
 OFFSET_Z = float(os.getenv("OFFSET_Z", "16000"))
