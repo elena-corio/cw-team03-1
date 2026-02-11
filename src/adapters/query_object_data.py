042ff6ef4bc6ba1e04fb37446af58680962ba87c
elena-code-star <elena.corio@students.iaac.net>
2026-02-11 18:44:25 +0100

    refactor query object data

--- src/adapters/query_object_data.py
+++ src/adapters/query_object_data.py
@@ -3,26 +3,25 @@ from gql import gql
 # Connect to Speckle's GraphQL API and fetches object data
 # from a specified project and object ID
 
-def query_object_data_graphql(client, project_id: str, object_id: str) -> dict:
-    #  Query object data from Speckle using GraphQL API.
-    query = gql("""
-    query GetObjectDataJSON($objectId: String!, $projectId: String!) {
-        project(id: $projectId) {
-            object(id: $objectId) {
-                id
-                speckleType
-                data
-            }
+GET_OBJECT_DATA_QUERY = gql("""
+query GetObjectDataJSON($objectId: String!, $projectId: String!) {
+    project(id: $projectId) {
+        object(id: $objectId) {
+            id
+            speckleType
+            data
         }
     }
-    """)
-    
-    variables = {
+}
+""")
+
+def query_object_data_graphql(client, project_id: str, object_id: str) -> dict:
+    """
+    Execute GraphQL query using the client's HTTP session
+    """
+    result = client.httpclient.execute(GET_OBJECT_DATA_QUERY, variable_values={
         "projectId": project_id,
         "objectId": object_id
-    }
-    
-    # Execute GraphQL query using the client's HTTP session
-    result = client.httpclient.execute(query, variable_values=variables)
+    })
     return result
 
