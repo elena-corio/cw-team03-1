49f93f24aad0fece0f2f2b2538e4d949a47109cc
elena-code-star <elena.corio@students.iaac.net>
2026-02-11 21:05:16 +0100

    Test adding both run backup updates and run receive and send data (new version) to main Problem: subsctiption is started but no updates are received

--- src/main.py
+++ src/main.py
@@ -1,11 +1,16 @@
 import asyncio
+from application.session3_workflow import run_receive_and_send_data_workflow
 from application.session4_workflow import run_backup_updates_workflow
 
+async def run_receive_and_send_data_workflow_async():
+    run_receive_and_send_data_workflow()
 
-def main():
-    run_backup_updates_workflow()
-    asyncio.run(run_backup_updates_workflow())
+async def main():
+    await asyncio.gather(
+        run_backup_updates_workflow(),
+        run_receive_and_send_data_workflow_async()
+    )
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
